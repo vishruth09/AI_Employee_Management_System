671d114be87b1356f6c8ac593147dbb74a23db36
vishruth <vishruth.palugula@gmail.com>
2026-07-08 21:38:32 +0530

    Conntected Frontend - Backend

--- AI_Employee_Management_System/main.py
+++ AI_Employee_Management_System/main.py
@@ -1,7 +1,18 @@
-def main():
-    print("Hello, AI Employee Management System!")
-    print("hello vishruth")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        return f"Username: {username}<br>Password: {password}"
+
+    return render_template("login.html")
+
 
 if __name__ == "__main__":
-    main()
-
+    app.run(debug=True)
